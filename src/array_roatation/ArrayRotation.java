a5c466768d990e412d8edad470b7e27272007a7e
Kevin Mckeon <kevmckeon@gmail.com>
2018-08-16 14:14:30 +0100

    Update Project : Optimise algorithm

--- src/array_roatation/ArrayRotation.java
+++ src/array_roatation/ArrayRotation.java
@@ -6,42 +6,70 @@ public class ArrayRotation {
 
 	public static void main(String[] args) {
 
-		ArrayList<Integer> array = new ArrayList<Integer>();
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.add(3);
+		arrayList.add(4);
+		arrayList.add(5);
+		arrayList.add(6);
+		arrayList.add(7);
+		arrayList.add(8);
+		arrayList.add(9);
 
-		int rotations = 6102;
+		int rotations = 4;
 
-		array.add(1);
-		array.add(2);
-		array.add(3);
-		array.add(4);
-		array.add(5);
+		// 	Method one - (Inefficient)
+		//	rotateLeftByMany(arrayList, rotations);
 
+		// 	Method two
+		 	modularRotate(arrayList, rotations);
+
+	}
+
+	private static void rotateLeftByMany(ArrayList<Integer> arrayList, int rotations) {
+		
 		for (int i = 0; i < rotations; i++) {
-			rotateLeftByOne(array);
-		}
+			
+			// Separate the left-most number
+			int temp = (int) arrayList.get(0);
 
-		System.out.println(array);
+			// Without moving out of bounds, move all numbers to the left by one
+			for (int j = 0; j < arrayList.size(); j++) {
 
-	}
-
-	private static void rotateLeftByOne(ArrayList array) {
-
-		// Separate the left-most number
-		int temp = (int) array.get(0);
-
-		// Without moving out of bounds, move all numbers to the left by one
-		for (int i = 0; i < array.size(); i++) {
-
-			if ((i) != array.size() - 1) {
-				array.set(i, (array.get(i + 1)));
-			} else {
-				break;
+				if ((j) != arrayList.size() - 1) {
+					arrayList.set(j, (arrayList.get(j + 1)));
+				} else {
+					break;
+				}
 			}
+
+			// Sub the temp back into the end of the array.
+			arrayList.set(arrayList.size() - 1, temp);
 		}
 
-		//Sub the temp back into the end of the array.
-		array.set(array.size() - 1, temp);
+		System.out.println(arrayList);
 
 	}
 
+	private static void modularRotate(ArrayList array, int rotations) {
+
+		// Create a new array
+		int[] newArray = new int[array.size()];
+
+		// Populate new array with rotations
+		for (int i = 0; i < array.size(); i++) {
+			int indexToMoveTo = (i + (array.size() - rotations)) % array.size();
+			newArray[indexToMoveTo] = (int) array.get(i);
+		}
+
+		// Convert new array back into ArrayList
+		ArrayList<Integer> arrayListRotated = new ArrayList<Integer>();
+
+		for (int x : newArray)
+			arrayListRotated.add(x);
+
+		System.out.println(arrayListRotated);
+
+	}
 }
